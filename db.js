425a087950c389db09ee537a5da09d6e0f23c359
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-20 19:01:26 -0700

    updated URI

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const {Client} = require('pg');
 let DB_URI;
 
 (process.env.NODE_ENV === "test") 
-? DB_URI = "postgresql:///users_test" 
-: DB_URI = "postgresql:///users";
+? DB_URI = "postgresql:///biztime_test" 
+: DB_URI = "postgresql:///biztime";
 
 let db = new Client({
     connectionString: DB_URI
