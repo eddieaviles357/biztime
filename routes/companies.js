52105932f66e8bcb98edd9d0890c43031222a3f9
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-20 19:30:13 -0700

    semicolons added

--- routes/companies.js
+++ routes/companies.js
@@ -4,10 +4,10 @@ const db = require('../db');
 /** GET /companies/ */
 router.get('/', async(req, res, next) => {
     try {
-        let queryResult = await db.query('SELECT * FROM companies')
-        res.status(200).json({companies: queryResult.rows})
+        let queryResult = await db.query('SELECT * FROM companies');
+        res.status(200).json({companies: queryResult.rows});
     } catch (err) {
-        next(err)
+        next(err);
     }
 })
 
