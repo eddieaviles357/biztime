5dbb4bc075545c6217b9617c2481ed97f8cec277
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-20 19:45:08 -0700

    get request route for company by code added

--- routes/companies.js
+++ routes/companies.js
@@ -1,13 +1,27 @@
 const router = require('express').Router();
 const db = require('../db');
+const ExpressError = require('../expressError');
 
 /** GET /companies/ */
 router.get('/', async(req, res, next) => {
     try {
-        let queryResult = await db.query('SELECT * FROM companies');
-        res.status(200).json({companies: queryResult.rows});
+        let queryRes = await db.query('SELECT * FROM companies');
+        return res.status(200).json({companies: queryRes.rows});
     } catch (err) {
-        next(err);
+        return next(err);
+    }
+});
+
+/** GET /companies/[code] */
+router.get('/:code', async(req, res, next) => {
+    try {
+        let {code} = req.params;
+        let queryRes = await db.query('SELECT code, name, description FROM companies WHERE code=$1', [code]);
+        /** Does not exist throw Error with a 404 status */
+        if(queryRes.rows[0] === undefined) throw new ExpressError(`${code} is not in DB`, 404); 
+        return res.status(200).json({company: queryRes.rows[0]});
+    } catch (err) {
+        return next(err)
     }
 })
 
