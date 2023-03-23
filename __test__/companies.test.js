40892c084406ff12bab3a86a0c506dc80bd8aa0b
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-22 19:21:47 -0700

    commented code

---
+++ __test__/companies.test.js
@@ -0,0 +1,13 @@
+// process.env.NODE_ENV = "test";
+
+// const request = require("supertest");
+
+// const app = require("../app");
+
+// beforeEach( async() => {
+
+// });
+
+// afterEach( async() => {
+    
+// });
