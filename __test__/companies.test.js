26a28757fe87f69608abca73d19845680732ae51
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-22 20:00:14 -0700

    GET /companies test added

--- __test__/companies.test.js
+++ __test__/companies.test.js
@@ -1,13 +1,55 @@
-// process.env.NODE_ENV = "test";
+/** jest companies.test.js */
+process.env.NODE_ENV = 'test';
 
-// const request = require("supertest");
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db');
 
-// const app = require("../app");
+let id;
+let code;
+let name;
+let description;
+beforeEach( async() => {
+    let companies = await db.query(
+        `INSERT INTO companies
+        VALUES 
+        ('bbraun', 'medical', 'liquid.')
+        RETURNING code, name, description`);
+    // assign data for testing
+    let res = companies.rows[0];
+    code = res.code;
+    name = res.name;
+    description = res.description;
+    let invoices = await db.query(
+        `INSERT INTO invoices (comp_code, amt, paid, paid_date)
+        VALUES 
+        ('bbraun', 100, false, null)
+        RETURNING id`);
+    /** assign id for testing */
+    id = invoices.rows[0].id;
+});
 
-// beforeEach( async() => {
+// clean up
+afterEach( async() => {
+    id = null;
+    code = null;
+    name = null;
+    description = null;
+    await db.query(`DELETE FROM invoices`);
+    await db.query(`DELETE FROM companies`);
+});
 
-// });
+// close the connection to db
+afterAll( async() => {
+    await db.end();
+});
 
-// afterEach( async() => {
-    
-// });
+describe('GET /companies', () => {
+    test('get all companies in db', async() => {
+        const res = await request(app).get('/companies');
+        let body = res.body
+
+        expect(res.statusCode).toBe(200);
+        expect(body).toHaveProperty('companies', [ { code, name, description } ] );
+    });
+});
