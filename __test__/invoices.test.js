14fa34c1541d26539ecafe87ae5dcff3228f70a7
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-25 19:30:18 -0700

    replaced add_date with add_date coming from db

--- __test__/invoices.test.js
+++ __test__/invoices.test.js
@@ -54,113 +54,113 @@ describe('GET /invoices', () => {
 });
 
 
-// describe('GET /invoices/:id', () => {
-//     test('get an invoice by id', async() => {
-//         const res = await request(app).get(`/invoices/${id}`);
-//         const body = res.body
+describe('GET /invoices/:id', () => {
+    test('get an invoice by id', async() => {
+        const res = await request(app).get(`/invoices/${id}`);
+        const body = res.body
 
-//         expect(res.statusCode).toBe(200);
-//         expect(body).toHaveProperty('invoice', {
-//             id,
-//             amt: 100,
-//             paid: false,
-//             add_date: '2023-03-22T07:00:00.000Z',
-//             paid_date: null,
-//             code: 'bbraun',
-//             name: 'medical',
-//             description: 'liquid.'
-//           });
-//     });
+        expect(res.statusCode).toBe(200);
+        expect(body).toHaveProperty('invoice', {
+            id,
+            amt: 100,
+            paid: false,
+            add_date,
+            paid_date: null,
+            code: 'bbraun',
+            name: 'medical',
+            description: 'liquid.'
+          });
+    });
 
-//     test('try getting an invoice by id with non existing id', async() => {
-//         const invId = 0;
-//         const res = await request(app).get(`/invoices/${invId}`);
-//         const body = res.body
+    test('try getting an invoice by id with non existing id', async() => {
+        const invId = 0;
+        const res = await request(app).get(`/invoices/${invId}`);
+        const body = res.body
 
-//         expect(res.statusCode).toBe(404);
-//         expect(body).toHaveProperty('error', {
-//             message: `Id: ${invId} does not exist in db`,
-//             status: 404
-//           });
-//     });
-// });
+        expect(res.statusCode).toBe(404);
+        expect(body).toHaveProperty('error', {
+            message: `Id: ${invId} does not exist in db`,
+            status: 404
+          });
+    });
+});
 
 
-// describe('POST /invoices', () => {
-//     test('add an invoice to db', async() => {
-//         const comp_code = 'bbraun';
-//         const amt = 100;
-//         const res = await request(app).post('/invoices').send( { comp_code, amt } );
-//         const body = res.body;
-//         expect(res.statusCode).toBe(201);
-//         expect(body).toHaveProperty('invoices', {
-//             id: id+1,
-//             comp_code,
-//             amt,
-//             paid: false,
-//             add_date: '2023-03-22T07:00:00.000Z',
-//             paid_date: null
-//         });
-//     });
+describe('POST /invoices', () => {
+    test('add an invoice to db', async() => {
+        const comp_code = 'bbraun';
+        const amt = 100;
+        const res = await request(app).post('/invoices').send( { comp_code, amt } );
+        const body = res.body;
+        expect(res.statusCode).toBe(201);
+        expect(body).toHaveProperty('invoices', {
+            id: id+1,
+            comp_code,
+            amt,
+            paid: false,
+            add_date,
+            paid_date: null
+        });
+    });
 
-//     test('try to add an invoice to a non existing company', async() => {
-//         const comp_code = 'invalid';
-//         const amt = 100;
-//         const res = await request(app).post('/invoices').send( { comp_code, amt } );
-//         const body = res.body;
-//         expect(res.statusCode).toBe(404);
-//         expect(body).toHaveProperty('error', { 
-//             message: 'Not present in db',
-//             status: 404
-//         });
-//     });
-// });
+    test('try to add an invoice to a non existing company', async() => {
+        const comp_code = 'invalid';
+        const amt = 100;
+        const res = await request(app).post('/invoices').send( { comp_code, amt } );
+        const body = res.body;
+        expect(res.statusCode).toBe(404);
+        expect(body).toHaveProperty('error', { 
+            message: 'Not present in db',
+            status: 404
+        });
+    });
+});
 
-// describe('PUT /invoices/:id', () => {
-//     test('update an existing invoice', async() => {
-//         const amt = 100;
-//         const res = await request(app).put(`/invoices/${id}`).send( { amt } );
-//         const body = res.body;
-//         expect(res.statusCode).toBe(200);
-//         expect(body).toHaveProperty('invoice', {
-//             id,
-//             comp_code: 'bbraun',
-//             amt,
-//             paid: false,
-//             add_date: '2023-03-22T07:00:00.000Z',
-//             paid_date: null
-//           });
-//     });
+describe('PUT /invoices/:id', () => {
+    test('update an existing invoice', async() => {
+        const amt = 100;
+        const res = await request(app).put(`/invoices/${id}`).send( { amt } );
+        const body = res.body;
+        expect(res.statusCode).toBe(200);
+        expect(body).toHaveProperty('invoice', {
+            id,
+            comp_code: 'bbraun',
+            amt,
+            paid: false,
+            add_date,
+            paid_date: null
+          });
+    });
 
-//     test('try to update an existing invoice with an invalid id', async() => {
-//         const amt = 100;
-//         const invId = 0;
-//         const res = await request(app).put(`/invoices/${invId}`).send( { amt } );
-//         const body = res.body;
-//         expect(res.statusCode).toBe(404);
-//         expect(body).toHaveProperty('error', { 
-//             message: `Id: ${invId} does not exist in db`,
-//             status: 404
-//         });
-//     });
-// });
+    test('try to update an existing invoice with an invalid id', async() => {
+        const amt = 100;
+        const invId = 0;
+        const res = await request(app).put(`/invoices/${invId}`).send( { amt } );
+        const body = res.body;
+        expect(res.statusCode).toBe(404);
+        expect(body).toHaveProperty('error', { 
+            message: `Id: ${invId} does not exist in db`,
+            status: 404
+        });
+    });
+});
 
-// describe('DELETE /invoices/:id', () => {
-//     test('delete an invoice', async() => {
-//         const res = await request(app).delete(`/invoices/${id}`);
-//         const body = res.body;
-//         expect(res.statusCode).toBe(200);
-//         expect(res.body).toHaveProperty('status', 'deleted');
-//     });
+describe('DELETE /invoices/:id', () => {
+    test('delete an invoice', async() => {
+        const res = await request(app).delete(`/invoices/${id}`);
+        const body = res.body;
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty('status', 'deleted');
+    });
 
-//     test('try to delete an invoice that does not exist', async() => {
-//         const invId = 0;
-//         const res = await request(app).delete(`/invoices/${invId}`);
-//         const body = res.body;
-//         expect(res.statusCode).toBe(404);
-//         expect(body).toHaveProperty('error', { 
-//             message: `Id: ${invId} does not exist in db`,
-//             status: 404
-//         });
-//     });
-// });
+    test('try to delete an invoice that does not exist', async() => {
+        const invId = 0;
+        const res = await request(app).delete(`/invoices/${invId}`);
+        const body = res.body;
+        expect(res.statusCode).toBe(404);
+        expect(body).toHaveProperty('error', { 
+            message: `Id: ${invId} does not exist in db`,
+            status: 404
+        });
+    });
+});
